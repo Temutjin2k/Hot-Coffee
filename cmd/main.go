84a536cf6f41238f9e4e11f3bd1d40b208005685
tkoszhan <tamutdzhin2006@mail.ru>
2024-10-22 12:49:20 +0500

    main beauty

--- cmd/main.go
+++ cmd/main.go
@@ -16,33 +16,38 @@ import (
 )
 
 func main() {
+	// Check for help flag
 	utils.Help(os.Args)
+
 	user, err := user.Current() // Создаем объект user, а уже из него достаем основную директорию user.HomeDir
 	if err != nil {
 		fmt.Println("Error getting user home directory")
 		os.Exit(1)
 	}
 
+	// Checking Flags
 	dir, port := config.Flagchecker()
 	path := filepath.Join(user.HomeDir, "hot-coffee", dir)
 
 	if !utils.DirectoryExists(path) {
 		utils.CreateDir(dir)
 	}
-	// Initialize repositories
+
+	// Initialize repositories (Data Access Layer)
 	menuRepo := dal.NewMenuRepository()
 	inventoryRepo := dal.NewInventoryRepository()
 	orderRepo := dal.NewOrderRepository()
 
-	// Initialize services
+	// Initialize services (Business Logic Layer)
 	menuService := service.NewMenuService(*menuRepo, *inventoryRepo)
 	orderService := service.NewOrderService(*orderRepo)
+	inventoryService := service.NewInventoryService(*inventoryRepo)
 
-	// Initialize handlers
+	// Initialize handlers (Presentation Layer)
 	menuHandler := handler.NewMenuHandler(menuService)
 	orderHandler := handler.NewOrderHandler(orderService)
-	inventoryHandler := handler.NewInventoryHandler()     // TODO
-	aggregationHandler := handler.NewAggregationHandler() // TODO
+	inventoryHandler := handler.NewInventoryHandler(inventoryService) // TODO
+	reportHandler := handler.NewAggregationHandler(orderService)
 
 	// Setup HTTP routes
 	mux := http.NewServeMux()
@@ -50,8 +55,11 @@ func main() {
 	mux.HandleFunc("/orders", orderHandler.OrderHandler)
 	mux.HandleFunc("/orders/", orderHandler.OrderHandler)
 	mux.HandleFunc("/inventory", inventoryHandler.InventoryHandler)
-	mux.HandleFunc("/reports/total-sales", aggregationHandler.TotalSalesHandler)
-	mux.HandleFunc("/reports/popular-items", aggregationHandler.PopularItemsHandler)
+	mux.HandleFunc("/reports/total-sales", reportHandler.TotalSalesHandler)
+	mux.HandleFunc("/reports/popular-items", reportHandler.PopularItemsHandler)
+
+	address := "http://localhost:" + port + "/"
+	fmt.Println("Server launched on address:", address)
 
 	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
