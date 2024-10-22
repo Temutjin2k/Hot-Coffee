ace351c0716aca75893e957068c6655d80d58911
tkoszhan <tamutdzhin2006@mail.ru>
2024-10-22 20:22:00 +0500

    Routing requests

--- cmd/main.go
+++ cmd/main.go
@@ -44,28 +44,45 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(logFile, nil))
 
 	// Initialize repositories (Data Access Layer)
+	orderRepo := dal.NewOrderRepository()
 	menuRepo := dal.NewMenuRepository()
 	inventoryRepo := dal.NewInventoryRepository()
-	orderRepo := dal.NewOrderRepository()
 
 	// Initialize services (Business Logic Layer)
-	menuService := service.NewMenuService(*menuRepo, *inventoryRepo)
 	orderService := service.NewOrderService(*orderRepo)
+	menuService := service.NewMenuService(*menuRepo, *inventoryRepo)
 	inventoryService := service.NewInventoryService(*inventoryRepo) // TODO
 
 	// Initialize handlers (Presentation Layer)
-	menuHandler := handler.NewMenuHandler(menuService, logger)
 	orderHandler := handler.NewOrderHandler(orderService, logger)
+	menuHandler := handler.NewMenuHandler(menuService, logger)
 	inventoryHandler := handler.NewInventoryHandler(inventoryService, logger) // TODO
 	reportHandler := handler.NewAggregationHandler(orderService, logger)
 
 	// Setup HTTP routes
 	mux := http.NewServeMux()
-	mux.HandleFunc("/menu", menuHandler.MenuHandler)
-	mux.HandleFunc("/orders", orderHandler.OrderHandler)
-	mux.HandleFunc("/inventory", inventoryHandler.InventoryHandler)
-	mux.HandleFunc("/reports/total-sales", reportHandler.TotalSalesHandler)
-	mux.HandleFunc("/reports/popular-items", reportHandler.PopularItemsHandler)
+
+	mux.HandleFunc("POST /orders", orderHandler.PostOrder)
+	mux.HandleFunc("GET /orders", orderHandler.GetOrder)
+	mux.HandleFunc("GET /orders/{id}", orderHandler.GetOrders)
+	mux.HandleFunc("PUT /orders/{id}", orderHandler.PutOrder)
+	mux.HandleFunc("DELETE /orders/{id}", orderHandler.DeleteOrder)
+	mux.HandleFunc("POST /orders/{id}/close", orderHandler.CloseOrder)
+
+	mux.HandleFunc("POST /menu", menuHandler.PostMenu)
+	mux.HandleFunc("GET /menu", menuHandler.GetMenu)
+	mux.HandleFunc("GET /menu/{id}", menuHandler.GetMenuItem)
+	mux.HandleFunc("PUT /menu/{id}", menuHandler.PutMenuItem)
+	mux.HandleFunc("DELETE /menu/{id}", menuHandler.DeleteMenuItem)
+
+	mux.HandleFunc("POST /inventory", inventoryHandler.PostInventory)
+	mux.HandleFunc("GET /inventory", inventoryHandler.GetInventory)
+	mux.HandleFunc("GET /inventory/{id}", inventoryHandler.GetInventoryItem)
+	mux.HandleFunc("PUT /inventory/{id}", inventoryHandler.PutInventoryItem)
+	mux.HandleFunc("DELETE /inventory/{id}", inventoryHandler.DeleteInventoryItem)
+
+	mux.HandleFunc("GET /reports/total-sales", reportHandler.TotalSalesHandler)
+	mux.HandleFunc("GET /reports/popular-items", reportHandler.PopularItemsHandler)
 
 	address := "http://localhost:" + port + "/"
 	fmt.Println("Server launched on address:", address)
