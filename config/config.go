8531983cf2aefb5a47cdb84b495e0bea0468442a
tkoszhan <tamutdzhin2006@mail.ru>
2024-10-22 11:06:38 +0500

    Changed default directory on --dir flag

--- config/config.go
+++ config/config.go
@@ -5,7 +5,7 @@ import "flag"
 var BaseDir string
 
 func Flagchecker() (string, string) {
-	dir := flag.String("dir", "maks", "Path to the data directory")
+	dir := flag.String("dir", "data", "Path to the data directory")
 	port := flag.String("port", "8080", "Port Number")
 	BaseDir = *dir
 	flag.Parse()
