407b11e9cac82c4fa79708e0ec23a1aacd2e3722
tkoszhan <tamutdzhin2006@mail.ru>
2024-10-22 21:23:15 +0500

    Log correct

--- internal/handler/aggregations_handler.go
+++ internal/handler/aggregations_handler.go
@@ -26,7 +26,7 @@ func (h *AggregationHandler) TotalSalesHandler(w http.ResponseWriter, r *http.Re
 	}
 	totalSales, err := h.orderService.GetTotalSales()
 	if err != nil {
-		h.logger.Error("Error in orderService GetTotalSales()", "error", err)
+		h.logger.Error("Error getting data", "error", err, "method", r.Method, "url", r.URL)
 		ErrorHandler.Error(w, "Error getting data", http.StatusInternalServerError)
 		return
 	}
@@ -39,13 +39,9 @@ func (h *AggregationHandler) TotalSalesHandler(w http.ResponseWriter, r *http.Re
 
 // Returns Each item as key and quatity as value
 func (h *AggregationHandler) PopularItemsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		ErrorHandler.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
 	popularItems, err := h.orderService.GetPopularItems(3)
 	if err != nil {
-		h.logger.Error("Error in orderService GetPopularItems", "error", err)
+		h.logger.Error("Error in orderService GetPopularItems", "error", err, "method", r.Method, "url", r.URL)
 		ErrorHandler.Error(w, "Error getting data", http.StatusInternalServerError)
 		return
 	}
